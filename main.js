adf85068f2d18d2a2c07191b758b3b424673841b
askhalsa <arshdeep.singh@crownstack.com>
2021-10-11 16:12:30 +0530

    complete javascript project

---
+++ main.js
@@ -0,0 +1,46 @@
+// const ul = document.querySelector('.items');
+
+// ul.firstElementChild.textContent = 'Hello';
+// ul.children[1].innerText = 'Brad';
+// ul.lastElementChild.innerHTML = 'welcome';
+
+// const btn= document.querySelector('.btn');
+// btn.style.background = 'red';
+
+// btn.addEventListener('click', (e) => {
+//     e.preventDefault();
+//     document.querySelector('#my-form')
+//     .style.background= '#ccc';
+//     document.querySelector('body').classList.add('bg-dark');
+//     document.querySelector('.items').lastElementChild.innerHTML= "hello"
+    
+// })
+
+const myForm= document.querySelector('#my-form');
+const nameInput= document.querySelector('#name');
+const emailInput= document.querySelector('#email');
+const msg= document.querySelector('.msg');
+const userList= document.querySelector('#users');
+
+myForm.addEventListener('submit', onSubmit);
+
+function onSubmit(e){
+    e.preventDefault();
+
+    if(nameInput.value === ''|| emailInput.value === '') {
+        msg.classList.add('error');
+        msg.innerHTML= 'Please enter all fields';
+
+        setTimeout(()=>msg.remove(),3000);
+    }else
+    {
+        const li = document.createElement('li');
+        li.appendChild(document.createTextNode(`${nameInput.value}:${emailInput.value}`));
+
+        userList.appendChild(li);
+
+        nameInput.value = '';
+        emailInput.value = '';
+    }
+}
+
